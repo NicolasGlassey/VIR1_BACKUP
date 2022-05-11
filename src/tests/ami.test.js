520b83fcea655a01338cdc5ea7922aaa95a35b52
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-05-11 10:36:00 +0200

    Create default tests for ami

---
+++ src/tests/ami.test.js
@@ -0,0 +1,29 @@
+"use strict";
+
+test('AMICreate_InstanceExist_RecivedAnAMIID', () => {
+    //given
+    //when
+    //getters are called directly in assertion below
+    //then
+})
+
+test('AMICreate_InstanceNotExist_ThrowErrorInvalidParameterValue', () => {
+    //given
+    //when
+    //getters are called directly in assertion below
+    //then
+})
+
+test('AMIDelete_AMINotExist_ThrowError', () => {
+    //given
+    //when
+    //getters are called directly in assertion below
+    //then
+})
+
+test('AMIDelete_AMIExist_Success', () => {
+    //given
+    //when
+    //getters are called directly in assertion below
+    //then
+})
