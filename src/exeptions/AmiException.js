56773ccb7c055e571ff5dca239c77659b28ded40
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-05-11 16:03:08 +0200

    :white_check_mark: Update AMICreate_InstanceExist_RecivedAnAMIID
    Create AMI and test AMI for an existing InstanceId

--- src/exeptions/AmiException.js
+++
@@ -1,2 +0,0 @@
-module.exports = class AmiException extends Error {
-}
--- src/tests/ami.test.js
+++ src/tests/ami.test.js
@@ -1,10 +1,28 @@
+/**
+ * Author : Anthony Bouillant
+ * Date : 11.02.2022
+ * Description : Test the AMI module
+ */
+
 "use strict";
 
-test('AMICreate_InstanceExist_RecivedAnAMIID', () => {
-    //given
-    //when
-    //getters are called directly in assertion below
-    //then
+// import { EC2Client, CreateImageCommand } from "@aws-sdk/client-ec2"; // ES Modules import
+const { EC2Client, CreateImageCommand } = require("@aws-sdk/client-ec2");
+const client = new EC2Client({ region: "eu-west-3" });
+
+
+test('AMICreate_InstanceExist_RecivedAnAMIID', async () => {
+
+    /** input parameters */
+    const input = {
+        'InstanceId': 'i-04199df6d81374547',
+        'Name': 'ami-test-17',
+    };
+
+    const command = new CreateImageCommand(input);
+    const response = await client.send(command);
+
+    expect(response.$metadata.httpStatusCode).toEqual(200);
 })
 
 test('AMICreate_InstanceNotExist_ThrowErrorInvalidParameterValue', () => {
