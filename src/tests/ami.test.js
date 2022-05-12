8d91d4278c0c3f0e05232a5d35f45d2c28aeecb8
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-05-12 08:59:14 +0200

    :white_check_mark: Update AmiTest
    Pass tests : AMICreate_InstanceExist_RecivedAnAMIID, AMICreate_InstanceNotExist_ThrowInvalidInstanceIDNotFound, AMICreate_InstanceNotExist_ThrowErrorInvalidParameterValue

--- src/tests/ami.test.js
+++ src/tests/ami.test.js
@@ -5,18 +5,20 @@
  */
 
 "use strict";
-
-// import { EC2Client, CreateImageCommand } from "@aws-sdk/client-ec2"; // ES Modules import
 const { EC2Client, CreateImageCommand } = require("@aws-sdk/client-ec2");
-const client = new EC2Client({ region: "eu-west-3" });
 
+var client;
+
+beforeAll(() => {
+    client = new EC2Client({ region: "eu-west-3" });
+});
 
 test('AMICreate_InstanceExist_RecivedAnAMIID', async () => {
-
     /** input parameters */
     const input = {
         'InstanceId': 'i-04199df6d81374547',
-        'Name': 'ami-test-17',
+        'Name': 'ami-jest-1',
+        'Description': 'ami created by jest',
     };
 
     const command = new CreateImageCommand(input);
@@ -25,11 +27,36 @@ test('AMICreate_InstanceExist_RecivedAnAMIID', async () => {
     expect(response.$metadata.httpStatusCode).toEqual(200);
 })
 
-test('AMICreate_InstanceNotExist_ThrowErrorInvalidParameterValue', () => {
-    //given
-    //when
-    //getters are called directly in assertion below
-    //then
+test('AMICreate_InstanceNotExist_ThrowInvalidInstanceIDNotFound', async () => {
+    const input = {
+        'InstanceId': 'i-04199df6d81374949',
+        'Name': 'ami-jest-1',
+        'Description': 'ami created by jest',
+    };
+    const command = new CreateImageCommand(input);
+
+    try {
+        await client.send(command)
+    } catch (error) {
+        expect(error.name).toEqual('InvalidInstanceID.NotFound');
+    }
+
+})
+
+test('AMICreate_InstanceNotExist_ThrowErrorInvalidParameterValue', async () => {
+    const input = {
+        'InstanceId': 'i-041ererererererer',
+        'Name': 'ami-jest-1',
+        'Description': 'ami created by jest',
+    };
+    const command = new CreateImageCommand(input);
+
+    try {
+        await client.send(command)
+    } catch (error) {
+        expect(error.name).toEqual('InvalidParameterValue');
+    }
+
 })
 
 test('AMIDelete_AMINotExist_ThrowError', () => {
