8ce54250fe6b79961b27ca79fe562376a54d67ed
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-05-12 16:15:51 +0200

    :bulb: Update comments for Ami

--- src/ami/Ami.js
+++ src/ami/Ami.js
@@ -2,6 +2,8 @@
  * @file      Ami.js
  * @brief     This class is used to manage an AMI from an instance.
  * @author    Created by Anthony Bouillant
+ * @date      2022-12-05
+ * 
  */
 
 "use strict";
@@ -10,16 +12,24 @@ const { CreateImageCommand, DeregisterImageCommand, DescribeImagesCommand } = re
 
 module.exports = class Ami {
 
+    // #region Private members
     #client;
     #ami;
+    // #endregion
 
+    // #region Public members
+    /**
+     * @brief This method constructs an Ami object.
+     * @param {EC2Client} client : the client used to communicate with the AWS API. 
+     */
     constructor(client) {
         this.#client = client;
     }
 
     /**
-     * Verify if the AMI exists.
-     * @returns 
+     * @brief This method is used to find an AMI.
+     * @param {string} amiName : the name of the AMI to find. 
+     * @returns {object} ami : the AMI found.
      */
     async find(amiName) {
         const config = {
@@ -35,6 +45,12 @@ module.exports = class Ami {
         return response.Images[0];
     }
 
+    /**
+     * @brief This method is used to create an AMI from an instance.
+     * @param {string} amiName : the name of the AMI to create. 
+     * @param  {string} instanceId : the instance ID from which the AMI will be created.
+     * @returns response : the response of the request.
+     */
     async create(amiName, instanceId) {
         const input = {
             'InstanceId': instanceId,
@@ -53,6 +69,11 @@ module.exports = class Ami {
         return response;
     }
 
+    /**
+     * @brief This method is used to delete an AMI.
+     * @param {string} imageId : the ID of the AMI to delete.
+     * @returns response : the response of the request.
+     */
     async delete(imageId) {
         const input = {
             'ImageId': imageId,
@@ -68,13 +89,22 @@ module.exports = class Ami {
     }
 
     /**
-     * @param {any} amiName
+     * @brief This method is used to set the ami.
+     * @param {Ami} ami : the ami to set.
      */
-    set ami(amiName) {
-        this.#ami = amiName
+    set ami(ami) {
+        this.#ami = ami
     }
 
+    /**
+     * @brief This method is used to get the ami.
+     * @returns {Ami} ami : this ami.
+     */
     get ami() {
         return this.#ami;
     }
+    // #endregion
+
+    // #region Private methods
+    // #endregion
 }
