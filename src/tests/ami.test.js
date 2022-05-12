9889cce56ff569e4182b37ab825b8750a8accc34
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-05-12 11:35:26 +0200

    :white_check_mark: Update Ami.test
    Pass tests : AMIDelete_AMIExist_Success, AMIDelete_AMINotExist_ThrowError

--- src/tests/ami.test.js
+++ src/tests/ami.test.js
@@ -5,15 +5,30 @@
  */
 
 "use strict";
-const { EC2Client, CreateImageCommand } = require("@aws-sdk/client-ec2");
+const { EC2Client, CreateImageCommand, DeregisterImageCommand, DescribeImagesCommand, DescribeImages } = require("@aws-sdk/client-ec2");
 
-var client;
+var client, imageId;
 
 beforeAll(() => {
     client = new EC2Client({ region: "eu-west-3" });
 });
 
 test('AMICreate_InstanceExist_RecivedAnAMIID', async () => {
+
+    // Check if the image is created
+    const config = {
+        'Filters': [
+            { 'Name': 'name', 'Values': ['ami-jest-1'] }
+        ]
+    }
+
+    var commandDescribeImages = new DescribeImagesCommand(config);
+    var response = await client.send(commandDescribeImages);
+    if (response.Images.length > 0 && response.Images[0].Name == 'ami-jest-1') {
+        imageId = response.Images[0].ImageId;
+        return;
+    };
+
     /** input parameters */
     const input = {
         'InstanceId': 'i-04199df6d81374547',
@@ -21,10 +36,19 @@ test('AMICreate_InstanceExist_RecivedAnAMIID', async () => {
         'Description': 'ami created by jest',
     };
 
-    const command = new CreateImageCommand(input);
-    const response = await client.send(command);
+    // Create the image
+    var command = new CreateImageCommand(input);
+    response = await client.send(command);
+    imageId = response.$metadata.imageId;
 
     expect(response.$metadata.httpStatusCode).toEqual(200);
+
+    // Get the image freshly created
+    response = await client.send(commandDescribeImages);
+    expect(response.Images[0].ImageId).toBeDefined();
+
+    // Set the image to be deregistered
+    imageId = response.Images[0].ImageId;
 })
 
 test('AMICreate_InstanceNotExist_ThrowInvalidInstanceIDNotFound', async () => {
@@ -59,16 +83,32 @@ test('AMICreate_InstanceNotExist_ThrowErrorInvalidParameterValue', async () => {
 
 })
 
-test('AMIDelete_AMINotExist_ThrowError', () => {
+
+test('AMIDelete_AMIExist_Success', async () => {
     //given
+    const input = {
+        'ImageId': imageId,
+    };
+    const command = new DeregisterImageCommand(input);
     //when
+    const response = await client.send(command);
     //getters are called directly in assertion below
     //then
+    expect(response.$metadata.httpStatusCode).toEqual(200);
 })
 
-test('AMIDelete_AMIExist_Success', () => {
+test('AMIDelete_AMINotExist_ThrowError', async () => {
+
     //given
+    const input = {
+        'ImageId': imageId,
+    };
+    const command = new DeregisterImageCommand(input);
     //when
-    //getters are called directly in assertion below
-    //then
+    try {
+        await client.send(command)
+    } catch (error) {
+        expect(error.name).toEqual('InvalidAMIID.Unavailable');
+    }
 })
+
