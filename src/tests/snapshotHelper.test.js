6dbcc16e8bb4fbfcf402e4ce815890c3301a0bdf
Noah <delgadono@etml.educanet2.ch>
2022-06-19 15:01:58 +0200

    Update snapshotHelper to test new function

--- src/tests/snapshotHelper.test.js
+++ src/tests/snapshotHelper.test.js
@@ -5,14 +5,13 @@
  */
 
 "use strict";
-const SnapshotNotFound = require("../snapshot/exceptions/SnapshotNotFoundException");
-const SnapshotAlreadyExist = require("../snapshot/exceptions/SnapshotAlreadyExistException");
-const SnapshotVolumeNotFound = require("../snapshot/exceptions/SnapshotVolumeNotFoundException");
-const SnapshotHelper = require("../snapshot/SnapshotHelper");
+const SnapshotNotFound = require("../exceptions/snapshot/SnapshotNotFoundException");
+const SnapshotAlreadyExist = require("../exceptions/snapshot/SnapshotAlreadyExistException");
+const SnapshotVolumeNotFound = require("../exceptions/snapshot/SnapshotVolumeNotFoundException");
+const SnapshotHelper = require("../helpers/SnapshotHelper");
 
 let clientRegionName, snapshotHelper;
 let snapshotName, volumeName;
-jest.setTimeout('10000');
 
 
 beforeAll(async () => {
@@ -20,8 +19,16 @@ beforeAll(async () => {
     snapshotHelper = new SnapshotHelper(clientRegionName);
     snapshotName = "";
     volumeName = "";
+    let listSnapshotsNames = [
+        "snapshot-jest-1",
+        "snapshot-jest-6",
+        "snapshot-jest-7"
+    ]
+    //await Promise.all(listAmiNames.map(async (name) => { await snapshotHelper.create('jspasjd', name); }));
     await snapshotHelper.create('jspasjd', 'snapshot-jest-1');
 });
+
+
 
 test('exist_SnapshotExist_Success', async () => {
     //given
@@ -80,8 +87,40 @@ test('create_VolumeNotExist_ThrowException', async () => {
     //then
     //Exception thrown
 })
+test('exist_AllSnapshot_Success', async () => {
+    //given
+    volumeName = 'jspasjd';
+    //when
+    const result = await snapshotHelper.findAllByVolume(volumeName);
+    //then
+    expect(result.length).toBe(1);
+}
+)
+test('hasMoreThanXSnapshotFromVolume_LessThanNumberOfSnapshot_Success', async () => {
 
-test('snapshotDelete_SnapshotExist_Success', async () => {
+    //given
+    volumeName = "jspasjd";
+    let numberOfSanpshot = 0;
+
+    //when
+    const result = await snapshotHelper.hasMoreThanXSnapshotFromVolume(volumeName, numberOfSanpshot);
+
+    //then
+    expect(result).toBe(true);
+})
+test('delete_AllSnapshotByVolume_Success', async()=>{
+    //given
+    volumeName= 'jspasjd';
+    
+    //when
+    await snapshotHelper.deleteAllByVolume(volumeName);
+    
+    //then
+    expect(await snapshotHelper.findAllByVolume(volumeName)).toEqual([]);
+
+
+})
+test('delete_SnapshotExist_Success', async () => {
     //given
     snapshotName = 'snapshot-jest-1';
     volumeName = 'jspasjd';
@@ -95,7 +134,7 @@ test('snapshotDelete_SnapshotExist_Success', async () => {
     expect(await snapshotHelper.exists(snapshotName)).toBe(false);
 })
 
-test('SnapshotDelete_SnapshotNotExist_ThrowError', async () => {
+test('delete_SnapshotNotExist_ThrowError', async () => {
     //given
     snapshotName = 'snapshot-jest-5';
     expect(await snapshotHelper.exists('snapshot-jest-5')).toBe(false);
@@ -106,13 +145,6 @@ test('SnapshotDelete_SnapshotNotExist_ThrowError', async () => {
     //then
     //Exception thrown
 })
-//test find all snapshot of a volume
-test('findAll_SnapshotExist_Success', async () => {
-    //given
-    volumeName = 'jspasjd';
-    //when
-    const result = await snapshotHelper.findAllByVolume(volumeName);
-    //then
-    expect(result.length).toBe(3);
-}
-)
+
+
+
